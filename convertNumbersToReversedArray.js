76f992214ee1065723561925ed33ac7a94f2f920
Andrew Jo <byeongminjo01@gmail.com>
2022-12-15 21:16:04 -0800

    First Commit

---
+++ convertNumbersToReversedArray.js
@@ -0,0 +1,7 @@
+//Convert number to reversed array of digits
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+//35231 => [1,3,2,5,3]
+
+function digitize(n) {
+    return String(n).split('').reverse().map(Number)
+};
---
+++ findTheCapitals.js
@@ -0,0 +1,5 @@
+//  Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var capitals = function (word) {
+    return word.match(/[A-Z]/g).map( x => {return word.indexOf(x)})
+};
