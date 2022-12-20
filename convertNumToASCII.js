adba927bd50384471fe91017292ae93126e11b6e
Andrew Jo <byeongminjo01@gmail.com>
2022-12-19 21:16:23 -0800

    convert a number to a ASCII letter

---
+++ convertNumToASCII.js
@@ -0,0 +1,7 @@
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+// a number will pass through the function and return out a string character.
+//ex: getChar(64) // return 'A'
+
+function getChar(number){
+    return String.toChar(number)
+};
