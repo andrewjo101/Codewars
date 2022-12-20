2ec1ef2d6801b75c85d4acc64765ba3ed6c58fda
Andrew Jo <byeongminjo01@gmail.com>
2022-12-19 21:19:56 -0800

    Convert DNA to RNA

--- DnaToRnaConversion.js
+++ DnaToRnaConversion.js
@@ -4,6 +4,6 @@ Create a function which translates a given DNA string into RNA.
 For example:"GCAT"  =>  "GCAU" **/
 
 function DNAtoRNA(dna) {
-    return dna.replace(/T/g , 'U')
+    return dna.replace(/T/g , 'U');
     
 }
